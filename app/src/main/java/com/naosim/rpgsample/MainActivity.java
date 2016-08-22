93ce10b22ecf5fff396bb8689050efbe12f9147c
naosim <nao_pillows@yahoo.co.jp>
2016-08-22 10:16:29 +0900

    リファクタ

--- app/src/main/java/com/naosim/rpgsample/MainActivity.java
+++ app/src/main/java/com/naosim/rpgsample/MainActivity.java
@@ -1,24 +1,18 @@
 package com.naosim.rpgsample;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.naosim.rpgmodel.android.FieldViewModelFactoryImpl;
+import com.naosim.rpgmodel.android.GamePadView;
 import com.naosim.rpgmodel.android.MessageViewModelImpl;
 import com.naosim.rpgmodel.android.sirokuro.DataSaveRepositoryAndroidImpl;
-import com.naosim.rpgmodel.android.GamePadView;
 import com.naosim.rpgmodel.lib.GameMain;
 import com.naosim.rpgmodel.lib.script.MessageScriptController;
 import com.naosim.rpgmodel.lib.value.Item;
@@ -27,7 +21,6 @@ import com.naosim.rpgmodel.lib.viewmodel.MessageViewModel;
 import com.naosim.rpgmodel.sirokuro.SirokuroGame;
 
 import java.util.Date;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     GameMain gameMain;
@@ -44,18 +37,23 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    static final String ASSET_HTML_URL = "file:///android_asset/www/index.html";
+    WebView createWebView() {
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.clearCache(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(ASSET_HTML_URL + "?date=" + new Date().getTime());
+        return webView;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getActionBar();
-        if(actionBar != null)actionBar.hide();
+        if(getActionBar() != null)getActionBar().hide();
+        if(getSupportActionBar() != null) getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
-
-        final MessageViewModel messageViewModel = new MessageViewModelImpl((TextView)findViewById(R.id.text), findViewById(R.id.nextIcon));
-        final MessageScriptController c = new MessageScriptController(messageViewModel);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -63,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 5000);
 
+        final MessageViewModel messageViewModel = new MessageViewModelImpl((TextView)findViewById(R.id.text), findViewById(R.id.nextIcon));
+        final MessageScriptController c = new MessageScriptController(messageViewModel);
         findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,20 +71,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        WebView webView = (WebView)findViewById(R.id.webView);
-        webView.clearCache(true);
-
         // GAME MAIN　生成
         this.gameMain = createGameMain(
-                new FieldViewModelFactoryImpl(webView),
+                new FieldViewModelFactoryImpl(createWebView()),
                 c,
                 getSharedPreferences("hoge", Context.MODE_PRIVATE)
         );
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/www/index.html?date=" + new Date().getTime());
-
+        // ゲームパッド
         GamePadView gamepadView = (GamePadView)findViewById(R.id.gamePadView);
+        gamepadView.getArrowPadView().setFieldViewModel(gameMain.getFieldViewModel());
         gamepadView.getAButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,41 +90,14 @@ public class MainActivity extends AppCompatActivity {
         gamepadView.getBButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final List<? extends Item> itemList =  gameMain.getItemList();
-                if(itemList.size() == 0) {
-                    Toast.makeText(MainActivity.this, "どうぐがありません", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                View view = LayoutInflater.from(v.getContext()).inflate(R.layout.view_item, null);
-                final AlertDialog alertDialog = new AlertDialog
-                        .Builder(v.getContext())
-                        .setView(view)
-                        .create();
-
-                ListView listView = (ListView)view.findViewById(R.id.itemListView);
-                listView.setAdapter(new ItemListAdapter(v.getContext(), itemList));
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                ItemSelectDialog.showItemListDialog(v.getContext(), gameMain.getItemList(), new ItemSelectDialog.OnItemSelectedListener() {
                     @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        ItemListAdapter<Item> adapter = (ItemListAdapter<Item>)parent.getAdapter();
-                        Item selectedItem = adapter.getItem(position);
-                        gameMain.onItemUsed(selectedItem);
-                        alertDialog.dismiss();
+                    public void onItemSelected(Item item) {
+                        gameMain.onItemUsed(item);
                     }
                 });
-
-                view.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        alertDialog.dismiss();
-                    }
-                });
-                alertDialog.show();
-
             }
         });
-        gamepadView.getArrowPadView().setFieldViewModel(gameMain.getFieldViewModel());
     }
 
     @Override
