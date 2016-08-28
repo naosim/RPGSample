1cd66c9e8486a66e5f69b5a94e37574cafc38aa0
naosim <nao_pillows@yahoo.co.jp>
2016-08-29 06:45:30 +0900

    activity分離

--- app/src/main/java/com/naosim/rpgsample/MainActivity.java
+++ app/src/main/java/com/naosim/rpgsample/MainActivity.java
@@ -2,25 +2,12 @@ package com.naosim.rpgsample;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.webkit.WebView;
-import android.widget.TextView;
 
-import com.naosim.rpglib.android.BGMPlayModelImpl;
-import com.naosim.rpglib.android.FieldViewModelFactoryImpl;
-import com.naosim.rpglib.android.GamePadView;
-import com.naosim.rpglib.android.ItemSelectDialogFactory;
-import com.naosim.rpglib.android.MessageViewModelImpl;
-import com.naosim.rpglib.android.SEPlayModelCore;
-import com.naosim.rpglib.android.SEPlayModelImpl;
+import com.naosim.rpglib.android.RPGBaseAbstractActivity;
 import com.naosim.rpglib.model.GameMain;
 import com.naosim.rpglib.model.script.MessageScriptController;
-import com.naosim.rpglib.model.value.Item;
 import com.naosim.rpglib.model.viewmodel.FieldViewModelFactory;
-import com.naosim.rpglib.model.viewmodel.MessageViewModel;
 import com.naosim.rpglib.model.viewmodel.sound.bgm.BGMPlayModel;
 import com.naosim.rpglib.model.viewmodel.sound.se.HasSE;
 import com.naosim.rpglib.model.viewmodel.sound.se.SEPlayModel;
@@ -32,18 +19,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-
-public class MainActivity extends AppCompatActivity {
-    GameMain gameMain;
-    WebView webView;
-    final ItemSelectDialogFactory itemSelectDialogFactory = new ItemSelectDialogFactory();
-    BGMPlayModelImpl bgmPlayModelImpl;
-    SEPlayModelImpl sePlayModelImpl;
-    SEPlayModelCore sePlayModelCore;
-
-    static GameMain createGameMain(
+public class MainActivity extends RPGBaseAbstractActivity {
+    @Override
+    public GameMain createGameMain(
             FieldViewModelFactory fieldViewModelFactory,
             MessageScriptController messageScriptController,
             SharedPreferences sharedPreferences,
@@ -59,8 +37,16 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    @Override
+    public List<HasSE> createSEList() {
+        List<HasSE> list = new ArrayList<>();
+        list.add(SirokuroSE.se1);
+        return list;
+    }
+
     static final String ASSET_HTML_URL = "file:///android_asset/www/index.html";
-    WebView createWebView() {
+    @Override
+    public WebView createWebView() {
         WebView webView = (WebView)findViewById(R.id.webView);
         webView.clearCache(true);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -68,116 +54,9 @@ public class MainActivity extends AppCompatActivity {
         return webView;
     }
 
-    SharedPreferences getSharedPreferences() {
+    @Override
+    public SharedPreferences getSharedPreferences() {
         return getSharedPreferences("hoge", Context.MODE_PRIVATE);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if(getActionBar() != null)getActionBar().hide();
-        if(getSupportActionBar() != null) getSupportActionBar().hide();
-
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                System.gc();
-            }
-        }, 5000);
-
-        final MessageViewModel messageViewModel = new MessageViewModelImpl((TextView)findViewById(R.id.text), findViewById(R.id.nextIcon));
-        final MessageScriptController c = new MessageScriptController(messageViewModel);
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                c.onNext();
-
-            }
-        });
-
-        this.webView = createWebView();
-        this.bgmPlayModelImpl =  new BGMPlayModelImpl(this);
-
-        List<HasSE> list = new ArrayList<>();
-        list.add(SirokuroSE.se1);
-        sePlayModelCore = new SEPlayModelCore(this, list);
-        sePlayModelImpl = new SEPlayModelImpl(sePlayModelCore);
-
-        // GAME MAIN　生成
-        this.gameMain = createGameMain(
-                new FieldViewModelFactoryImpl(this.webView),
-                c,
-                getSharedPreferences(),
-                this.bgmPlayModelImpl,
-                sePlayModelImpl
-        );
-
-        // ゲームパッド
-        GamePadView gamepadView = (GamePadView)findViewById(R.id.gamePadView);
-        gamepadView.getArrowPadView().setFieldViewModel(gameMain.getFieldViewModel());
-        gamepadView.getAButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gameMain.onPressAButton();
-            }
-        });
-        gamepadView.getBButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                itemSelectDialogFactory.showItemListDialog(v.getContext(), gameMain.getItemList(), new Function1<Item, Unit>() {
-                    @Override
-                    public Unit invoke(Item item) {
-                        gameMain.onItemUsed(item);
-                        return null;
-                    }
-                });
-            }
-        });
-
-        gamepadView.getSettingButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean newIsOn = !bgmPlayModelImpl.isOn();
-                bgmPlayModelImpl.setOn(newIsOn);
-                getSharedPreferences().edit().putBoolean("isBGMOn", newIsOn).commit();
-                getSharedPreferences().edit().putBoolean("isSEOn", newIsOn).commit();
-//                sePlayModelCore.play(SETest.se1);
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        gameMain.onStart();
-        bgmPlayModelImpl.setOn(getSharedPreferences().getBoolean("isBGMOn", true));
-        sePlayModelImpl.setOn(getSharedPreferences().getBoolean("isSEOn", true));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.resumeTimers();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        this.gameMain.onDestroy();
-        webView.pauseTimers();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        gameMain.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        this.gameMain.onDestroy();
-        sePlayModelCore.release();
-    }
 }
